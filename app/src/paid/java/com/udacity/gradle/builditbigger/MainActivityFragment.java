795db40e59987b238c2b63595014312d1005844e
Erik Cox <erikbcox@gmail.com>
2017-01-09 15:57:52 -0800

    Fix progress bar import bug for paid flavor

--- app/src/paid/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/paid/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ProgressBar;
 
 import rocks.ecox.jokeactivity.JokeActivity.JokeActivity;
 
