bcbd3790221e0be9a1a613552bf6be598d6dbc85
Erik Cox <erikbcox@gmail.com>
2017-01-09 10:26:58 -0800

    Add jokes to joke provider

--- jokelib/src/main/java/com/example/JokeProvider.java
+++ jokelib/src/main/java/com/example/JokeProvider.java
@@ -1,5 +1,23 @@
 package com.example;
 
+import java.util.Random;
+
 public class JokeProvider
 {
+    private static final String[] jokes = {"Why do programmers always mix up Halloween and Christmas? Because Oct 31 == Dec 25!",
+            "\"Knock, knock.\" \"Who’s there?\" <very long pause….> \"Java.\"",
+            "A SQL query goes into a bar, walks up to two tables and asks, \"Can I join you?\"",
+            "How many programmers does it take to change a light bulb? None, that's a hardware problem.",
+            "Whats the object-oriented way to become wealthy? Inheritance.",
+            "Unix is user friendly. It's just very particular about who its friends are."
+    };
+
+    public static String getJoke(){
+        int size = jokes.length;
+
+        Random r = new Random();
+        int index = r.nextInt(size);
+
+        return jokes[index];
+    }
 }
